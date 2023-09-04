f9bce94bbebde059a41bd3f64655049792e6f780
ethan-yang <89763726+Tofyu@users.noreply.github.com>
2023-09-03 17:54:16 -0700

    Update ResFeedbackScreen.js

--- screens/ResFeedbackScreen.js
+++ screens/ResFeedbackScreen.js
@@ -1,98 +1,77 @@
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
-import { TextInput, Button, } from 'react-native-paper';
-import React, {useState, useEffect} from 'react'
-import DropDownPicker from 'react-native-dropdown-picker';
-import { collection, addDoc, getDocs } from "firebase/firestore"; 
+import { TextInput, Button } from 'react-native-paper';
+import React, { useState } from 'react'
+import { collection, addDoc } from "firebase/firestore"; 
 import { db } from '../firebase';
 
-const ResFeedbackScreen = ( {route, navigation} ) => {
-    const [comments, setComments] = useState('')
-    const { eventID } = route.params;
+const ResFeedbackScreen = ({ route, navigation }) => {
+  const [comments, setComments] = useState('')
+  const { eventID } = route.params;
 
-    // const [events, setEvents] = useState([])
-    // const [open, setOpen] = useState(false);
-    // const [value, setValue] = useState([]);
-    
+  const send = async () => {
+    try {
+      const docRef = await addDoc(collection(db, "events", eventID, 'feedback'), {
+        comments: comments,
+        userID: "userID"
+      });
 
-    // const fetchData = async () => {
-    //     const querySnapshot = await getDocs(collection(db, "events"));
-    //     const docsData = querySnapshot.docs.map((doc) => ({label: doc.data().name, value: doc.id}));
-    //     setEvents(docsData);
-    //   };
-    
-    // useEffect(()=>{
-    //   fetchData()
-    // }, [])
-    
- 
-    const send = async ()=>{
-      try {
-        const docRef = await addDoc(collection(db, "events", eventID, 'feedback'), {
-          comments: comments,
-          userID: "userID"
-         
-        });
-        
-        navigation.navigate("Community Events")
-        console.log("Document written with ID: ", docRef.id);
-      } catch (e) {
-        console.error("Error adding document: ", e);
-      }
+      navigation.navigate("Community Events")
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
     }
-    
-      return (
-        <SafeAreaView style={styles.container}>
-                   
-          {/* <DropDownPicker
-                open={open}
-                value={value}
-                items={events}
-                setOpen={setOpen}
-                setValue={setValue}
-                // setItems={setItems}
-                theme="DARK"
-                multiple={false}
-                mode="BADGE"
-                badgeDotColors={["#e76f51", "#00b4d8", "#e9c46a", "#e76f51", "#8ac926", "#00b4d8", "#e9c46a"]}
-              />           */}
+  }
 
-        <View style={styles.inputContainer}>
-          <TextInput style={styles.input} placeholder='Give us your feedback!' onChangeText={comments => setComments(comments)} />
-
-          <Button onPress={send}> Send </Button>
-          </View>  
-        </SafeAreaView>
-      );
+  return (
+    <SafeAreaView style={styles.container}>
+        <TextInput
+          style={styles.input}
+          placeholder='Give us your feedback!'
+          multiline={true} // Allow multiple lines
+          numberOfLines={10} // Set the initial number of lines
+          
+          onChangeText={comments => setComments(comments)}
+        />
+       <Button onPress={send} style={styles.sendButton} labelStyle={styles.sendButtonText}>
+  Send
+</Button>
+    </SafeAreaView>
+  );
 }
 
 export default ResFeedbackScreen
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 100,
-        paddingHorizontal: 16,
-        paddingVertical: 8,
-        backgroundColor: '#F5F5F5',
-      },
-      inputContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        backgroundColor: 'white',
-        borderRadius: 10,
-        paddingHorizontal: 8,
-        paddingVertical: 8,
-        marginBottom: 8,
-      },
-      input: {
-        flex: 30,
-        fontSize: 16,
-        marginLeft: 8,
-        height: 120,
-      },
-      sendButton: {
-        backgroundColor: '#007AFF',
-        borderRadius: 10,
-        paddingVertical: 8,
-        paddingHorizontal: 12,
-      },
-})
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    backgroundColor: '#F5F5F5',
+  },
+  inputContainer: {
+    flexDirection: 'column', // Stack items vertically
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderRadius: 10,
+    paddingHorizontal: 8,
+    paddingVertical: 8,
+    marginBottom: 8,
+  },
+  input: {
+    fontSize: 16,
+    height: 400, // Increase the height as needed
+    width: '100%', // Take up the entire width
+    marginVertical: 8, // Separate from the button
+  },
+  sendButton: {
+    backgroundColor: '#9370DB', // Light purple color
+    borderRadius: 10,
+    paddingVertical: 8,
+    paddingHorizontal: 12,
+    marginTop: 8, // Separation from the input
+  },
+  sendButtonText: {
+    color: 'white', // White text color
+  }
+  
+});
