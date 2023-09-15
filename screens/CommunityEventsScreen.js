79d74a9ae0d9d3864918b1a6d5e078d9db16634e
ethan-yang <89763726+Tofyu@users.noreply.github.com>
2023-09-14 21:42:21 -0700

    Update CommunityEventsScreen.js

--- screens/CommunityEventsScreen.js
+++ screens/CommunityEventsScreen.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { Text, StyleSheet, View, FlatList, SafeAreaView } from 'react-native';
+import { StyleSheet, View, FlatList, SafeAreaView } from 'react-native';
 import { collection, getDocs, getDoc, doc, query, where } from 'firebase/firestore';
 import { db, auth } from '../firebase';
-import { Avatar, Card, IconButton, Button } from 'react-native-paper';
+import { Text, Avatar, Card, IconButton, Button, Divider } from 'react-native-paper';
 
 const CommunityEventsScreen = ({ navigation }) => {
   const [upcomingEvents, setUpcomingEvents] = useState([]);
   const [pastEvents, setPastEvents] = useState([]);
   const [communityUser, setCommunityUser] = useState()
+  const [communityName, setCommunityName] = useState('')
   const user = auth.currentUser;
 
   const getCommunityGroupID = async () => {
@@ -18,6 +19,7 @@ const CommunityEventsScreen = ({ navigation }) => {
     if (docSnap.exists()) {
       groupID = docSnap.data().communityID;
       setCommunityUser(docSnap.data().name)
+      setCommunityName(docSnap.data().communityName)
     } else {
       console.log('No such document!');
     }
@@ -27,33 +29,33 @@ const CommunityEventsScreen = ({ navigation }) => {
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [navigation]);
 
   const fetchData = async () => {
     let eventsFromDB = [];
     const groupID = await getCommunityGroupID();
-    const eventsQuery = query(collection(db, 'events'), where('communityID', '==', groupID));
+    const eventsQuery = query(collection(db, 'events'), where('communityID', '==', groupID), where('status', '==', 'accepted'));
     const querySnapshot = await getDocs(eventsQuery);
     const docsData = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-  
+
     for (const docSnap of docsData) {
       if (docSnap.communityID === groupID) {
         const songsCollectionRef = collection(db, 'events', docSnap.id, 'songs');
         const songsSnapshot = await getDocs(songsCollectionRef);
         const songsData = songsSnapshot.docs.map((songDoc) => songDoc.data());
-  
+
         eventsFromDB.push({ id: docSnap.id, ...docSnap, songsData });
       }
     }
-  
+
     const sortedData = eventsFromDB.sort((a, b) => new Date(a.dateTime) - new Date(b.dateTime));
-  
+
     const currentDateTime = new Date();
     const upcoming = [];
     const past = [];
-  
+
     for (const event of sortedData) {
-      const eventDateTime = event.dateTime.toDate();  
+      const eventDateTime = event.dateTime.toDate();
 
       if (eventDateTime <= currentDateTime) {
         past.push(event);
@@ -61,14 +63,14 @@ const CommunityEventsScreen = ({ navigation }) => {
         upcoming.push(event);
       }
     }
-  
+
     const sortedUpcoming = upcoming.sort((a, b) => new Date(a.dateTime) - new Date(b.dateTime));
     const sortedPast = past.sort((a, b) => new Date(b.dateTime) - new Date(a.dateTime));
-  
+
     setUpcomingEvents(sortedUpcoming);
     setPastEvents(sortedPast);
   };
-  
+
 
   const renderItem = ({ item }) => {
     const currentDateTime = new Date();
@@ -90,7 +92,7 @@ const CommunityEventsScreen = ({ navigation }) => {
     }
 
     return (
-      <Card>
+      <Card style={{ margin: 5 }}>
         <Card.Title
           title={item.volunteerGroupName}
           subtitle={eventDateTime.toLocaleString([], {
@@ -109,12 +111,20 @@ const CommunityEventsScreen = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.container}>
       {/* Welcome Text */}
+
       <Text style={styles.welcomeText}>Welcome, {communityUser}!</Text>
-      
-      <Button onPress={() => navigation.navigate('Community Register')}>
-          Register Your Community
-        </Button>
-      
+      {/* Show community name if there is, otherwise show create buttom */}
+      <View style={{ alignItems: 'flex-end', margin: 10 }}>
+        {communityName === null ? (
+          <Button icon="home-group-plus" mode="elevated" onPress={() => navigation.navigate('Community Register', { userID: user.uid })}>
+            Register Your Community
+          </Button>
+        ) : (
+          <Text variant="titleMedium">{communityName}</Text>
+        )}
+      </View>
+      <Divider />
+
       {/* Upcoming Events */}
       <FlatList
         data={upcomingEvents}
@@ -123,7 +133,7 @@ const CommunityEventsScreen = ({ navigation }) => {
         contentContainerStyle={styles.listContent}
         ListHeaderComponent={<Button>Upcoming Events</Button>}
       />
-      
+
       {/* Past Events */}
       <FlatList
         data={pastEvents}
@@ -144,9 +154,10 @@ const styles = StyleSheet.create({
     paddingTop: 20,
   },
   welcomeText: {
-    fontSize: 24,
+    fontSize: 20,
     fontWeight: 'bold',
     marginBottom: 10,
+    marginHorizontal: 10
   },
   listContent: {
     paddingBottom: 20,
